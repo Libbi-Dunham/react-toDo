6936fffab7da27e73aedf50cb2ee22c09b2b054e
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-11 14:26:38 -0800

    wrote out authform page

---
+++ src/components/AuthForm/AuthForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function AuthForm({ email, setEmail, password, error, setPassword, handleSubmit }) {
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h3>{error}</h3>
+      <div className="form-control">
+        <label>Email: </label>
+        <input
+          className="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Password: </label>
+        <input
+          className="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <input type="submit" />
+    </form>
+  );
+}
