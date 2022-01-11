f98fd7e330ffbdb33d8709b6870caee2a978ddb0
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-01-11 14:54:54 -0800

    got everything transfered over so original signin signup is working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
+import { getUser, logout } from './services/users';
+import Auth from './Views/Auth/Auth';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            {currentUser && (
+              <>
+                <h1>I am signed in</h1>
+                <button className="log" onClick={logoutUser}>
+                  Log Out
+                </button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
